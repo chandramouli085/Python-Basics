0e1b87f601d3b309686c09be908d1864bdaf3183
dropTableUsers42 <rwitaban0001@gmail.com>
2021-07-10 02:38:00 +0530

    added boilerplate

---
+++ SlowMatrix.py
@@ -0,0 +1,27 @@
+class SlowMatrix:
+	def __init__(self, matrix):
+		pass
+
+	def __mul__(self, mat2):
+		pass
+
+	def __add__(self, mat2):
+		pass
+
+	def __sub__(self, mat2):
+		pass
+
+	def __eq__(self, mat2):
+		pass
+
+	def inverse(self):
+		pass
+
+	def transpose(self):
+		pass
+
+	def ones(self):
+		pass
+
+	def zeros(self):
+		pass
